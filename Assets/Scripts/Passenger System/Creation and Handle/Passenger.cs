9a80bdd537b33820078fc6590726ab91de32cefe
Joaquin Incio <joaquin.incio2002@outlook.com>
2026-02-24 23:58:52 -0500

    Passenger Creation done
    Passenger System handles a queue that gets populated by a convertion from json data to passengers objects

---
+++ Assets/Scripts/Passenger System/Creation and Handle/Passenger.cs
@@ -0,0 +1,31 @@
+using System.Text;
+
+[System.Serializable]
+public struct Passenger
+{
+    public int skinId;
+    public float tolerance;
+    public int arrivalStop;
+    public int destinationStop;
+    public string initialMessage;
+    public string overpricedGoodReactionMessage;
+    public string overpricedBadReactionMessage;
+    public string kickOutReactionMessage;
+
+    public override string ToString()
+    {
+        StringBuilder sb = new StringBuilder();
+
+        sb.AppendLine("Passenger:");
+        sb.AppendLine($"  SkinId: {skinId}");
+        sb.AppendLine($"  Tolerance: {tolerance}");
+        sb.AppendLine($"  ArrivalStop: {arrivalStop}");
+        sb.AppendLine($"  DestinationStop: {destinationStop}");
+        sb.AppendLine($"  InitialMessage: {initialMessage}");
+        sb.AppendLine($"  OverpricedGoodReactionMessage: {overpricedGoodReactionMessage}");
+        sb.AppendLine($"  OverpricedBadReactionMessage: {overpricedBadReactionMessage}");
+        sb.AppendLine($"  KickOutReactionMessage: {kickOutReactionMessage}");
+
+        return sb.ToString();
+    }
+}
---
+++ Assets/Scripts/Passenger System/Creation and Handle/PassengerCreator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public struct Passengers
+{
+    public Passenger[] passengers;
+}
+
+public class PassengerCreator : MonoBehaviour
+{
+    public Queue<Passenger> CreatePassengerQueue(TextAsset PassengerJSON)
+    {
+        Queue<Passenger> queue = new Queue<Passenger>();
+
+        Passengers deserializedPassengers = JsonUtility.FromJson<Passengers>(PassengerJSON.text);
+
+        foreach(Passenger person in deserializedPassengers.passengers)
+        {
+            queue.Enqueue(person);
+        }
+
+        return queue;
+    }
+}
